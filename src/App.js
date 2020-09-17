f23ec02c07b44b2ae2e9884e5c99ac975a9fa53e
anadev <aluiza.silva22@gmail.com>
2020-09-17 19:08:50 -0400

    cool layout update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,83 @@
 import React from "react";
-import { Router } from "@reach/router";
-import { Header } from "./components/Header";
+import { Router, Link } from "@reach/router";
 import { Projects } from "./pages/Projects";
 import { Curriculum } from "./pages/Curriculum";
+import { Quiz } from "./pages/Quiz";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Router className="full_section container">
-        <Projects path="/" />
-        <Curriculum path="cv" />
-      </Router>
-    </div>
+  	<>
+	    <main className="App">
+	      <aside>
+		    <section>
+				<h2>Skills</h2>
+				<p>CSS 
+					<progress max="100" value="90">
+						<div class="progress-bar">
+			                <span style={{width:'90%'}}>90%</span>
+			            </div>
+			        </progress>
+				</p>
+				<p>HTML 
+					<progress max="100" value="90">
+						<div class="progress-bar">
+			                <span style={{width:'90%'}}>90%</span>
+			            </div>
+			        </progress>
+				</p>
+				<p>JS 
+					<progress max="100" value="70">
+						<div class="progress-bar">
+			                <span style={{width:'70%'}}>70%</span>
+			            </div>
+			        </progress>
+				</p>
+				<p>React 
+					<progress max="100" value="50">
+						<div class="progress-bar">
+			                <span style={{width:'50%'}}>50%</span>
+			            </div>
+			        </progress>
+				</p>
+				<p>PHP 
+					<progress max="100" value="70">
+						<div class="progress-bar">
+			                <span style={{width:'70%'}}>70%</span>
+			            </div>
+			        </progress>
+				</p>
+				<p>Wordpress 
+					<progress max="100" value="80">
+						<div class="progress-bar">
+			                <span style={{width:'80%'}}>80%</span>
+			            </div>
+			        </progress>
+				</p>
+				<p>Magento 
+					<progress max="100" value="80">
+						<div class="progress-bar">
+			                <span style={{width:'80%'}}>80%</span>
+			            </div>
+			        </progress>
+				</p>
+				<p>Ajax | APIs REST | Bootstrap | CakePHP | Cordova | Git | JQuery | JSON | Linux | MongoDB | MVC | MySQL | npm | Phonegap | Photoshop | PostgreSQL | React Native | Ruby on Rails | SASS | SCRUM | SEO | SVN | Trello | Web services | Windows | WooCommerce | Vue.js | XML</p>
+			</section>
+	      </aside>
+	      <Router>
+	        <Projects path="/" />
+	        <Curriculum path="cv" />
+	        <Quiz path="quiz" />
+	      </Router>
+	    </main>
+	    <footer>
+			<nav>
+				<Link to="/">projects</Link>
+				<Link to="cv">curriculum</Link>
+				<Link to="quiz">are we a good fit?</Link>
+			</nav>
+		</footer>
+	</>
   );
 }
 
---
+++ src/pages/Quiz.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+export const Quiz = () => {
+  return (   
+      <section id="QuizPage">
+        <h2>Are we a good fit?</h2>
+      </section>
+  );
+}
+
+export default Quiz;
